448329dfaeef39c13387e7fdbcd6ee8ee53a7d34
Parth Dedaniya <parth.dedaniya@simformsolutions.com>
2023-02-09 15:27:47 +0530

    Implement product edit and delete functionalities

--- crud-operation/script.js
+++ crud-operation/script.js
@@ -6,6 +6,9 @@ selected = document.getElementsByClassName("navbar");
 const content = document.getElementById("content");
 const editbtn = document.getElementsByClassName("editbtn");
 
+let products = JSON.parse(localStorage.getItem("products")) || [];
+
+
 window.addEventListener("load", async (e) => {
     const res = await fetch(routes[e.target.location.hash]);
     const data = await res.text();
@@ -13,21 +16,9 @@ window.addEventListener("load", async (e) => {
     if (e.target.location.hash == "#allProducts" || e.target.location.hash == "") {
         showData();
     } else if (e.target.location.hash == "#addProduct") {
-        document.getElementById("productForm").addEventListener("submit", (e) => {
-            e.preventDefault();
-            let productID = e.target.productID.value;
-            let productName = e.target.productName.value;
-            let productPrice = e.target.productPrice.value;
-            let productDescription = e.target.productDescription.value;
-            let product = {
-                id: productID,
-                name: productName,
-                price: productPrice,
-                description: productDescription
-            }
-            storeProduct(product);
-            window.location.href = "/";
-        })
+        addNewProduct();
+    } else if (e.target.location.hash == "#editProduct") {
+        editfunction(localStorage.getItem("editProductId"));
     }
 });
 
@@ -38,21 +29,9 @@ window.addEventListener("popstate", async (e) => {
     if (e.target.location.hash == "#allProducts" || e.target.location.hash == "") {
         showData();
     } else if (e.target.location.hash == "#addProduct") {
-        document.getElementById("productForm").addEventListener("submit", (e) => {
-            e.preventDefault();
-            let productID = e.target.productID.value;
-            let productName = e.target.productName.value;
-            let productPrice = e.target.productPrice.value;
-            let productDescription = e.target.productDescription.value;
-            let product = {
-                id: productID,
-                name: productName,
-                price: productPrice,
-                description: productDescription
-            }
-            storeProduct(product);
-            window.location.href = "/";
-        })
+        addNewProduct();
+    } else if (e.target.location.hash == "#editProduct") {
+        editfunction(localStorage.getItem("editProductId"));
     }
 })
 
@@ -60,7 +39,8 @@ const routes = {
     "": "./pages/allproducts.html",
     "#": "./pages/allproducts.html",
     "#allProducts": "./pages/allproducts.html",
-    "#addProduct": "./pages/addproduct.html"
+    "#addProduct": "./pages/addproduct.html",
+    "#editProduct": "./pages/editproduct.html"
 }
 
 Array.from(selected).forEach(element => {
@@ -71,47 +51,80 @@ Array.from(selected).forEach(element => {
         if (e.target.attributes.href.nodeValue == "#allProducts" || e.target.attributes.href.nodeValue == "#") {
             showData();
         } else if (e.target.attributes.href.nodeValue == "#addProduct") {
-            document.getElementById("productForm").addEventListener("submit", (e) => {
-                e.preventDefault();
-                let productID = e.target.productID.value;
-                let productName = e.target.productName.value;
-                let productPrice = e.target.productPrice.value;
-                let productDescription = e.target.productDescription.value;
-                let product = {
-                    id: productID,
-                    name: productName,
-                    price: productPrice,
-                    description: productDescription
-                }
-                storeProduct(product);
-                window.location.href = "/";
-            })
+            addNewProduct();
         }
     })
 })
 
-
-let products = JSON.parse(localStorage.getItem("products")) || [];
-
-
-function storeProduct(product) {
-    products.push(product);
-    localStorage.setItem("products", JSON.stringify(products));
+function addNewProduct() {
+    document.getElementById("productForm").addEventListener("submit", (e) => {
+        e.preventDefault();
+        let productID = e.target.productID.value;
+        let productName = e.target.productName.value;
+        let productPrice = e.target.productPrice.value;
+        let productDescription = e.target.productDescription.value;
+        let product = {
+            id: productID,
+            name: productName,
+            price: productPrice,
+            description: productDescription
+        }
+        products.push(product);
+        localStorage.setItem("products", JSON.stringify(products));
+        window.location.href = "/";
+    })
 }
 
 function showData() {
     let data = "";
-    products.map((product) => {
-        data+=`<tr>
-        <td>${product.id}</td>
-        <td>${product.name}</td>
-        <td>Image</td>
-        <td>${product.price}</td>
-        <td>${product.description}</td>
-        <td><button type="button" class="editbtn">Edit</button></td>
-        <td><button type="button" class="deletebtn">Delete</button></td>
-    </tr>`
-    })
+    products.length < 1 ? data = `<tr><td colspan="6" class="producterror">No Products Available!</td></tr>` : (
+        products.map((product) => {
+            data += `<tr>
+                <td>${product.id}</td>
+                <td>${product.name}</td>
+                <td>Image</td>
+                <td>${product.price}</td>
+                <td>${product.description}</td>
+                <td><button type="button" class="editbtn" onclick="editproduct(this)">Edit</button></td>
+                <td><button type="button" class="deletebtn" onclick="deleteproduct(this)">Delete</button></td>
+            </tr>`;
+    }))
     document.getElementById("product-list").innerHTML = data;
-    //productlist.innerHTML = data;
+}
+
+function deleteproduct(e) {
+    p = e.parentElement.parentElement.children[0].textContent;
+    let copyproducts = [...products];
+    copyproducts = copyproducts.filter(cp => cp.id !== p);
+    products = copyproducts;
+    localStorage.setItem("products", JSON.stringify(products));
+    window.location.href = "/";
+}
+
+function editproduct(e) {
+    window.location.hash = "#editProduct";
+    p = e.parentElement.parentElement.children[0].textContent;
+    let p_id = products.findIndex(product => product.id == p);
+    localStorage.setItem("editProductId", p_id);                //store edit-product-id in local storage to be able retrieve when reload
+    editfunction(p_id);
+}
+
+const editfunction = (pid) => {
+    setTimeout(() => {                                          //setTimeout to wait for get the form id
+        let editproduct = document.getElementById("editproductForm");
+        editproduct.elements.productID.value = products[pid].id;
+        editproduct.elements.productName.value = products[pid].name;
+        editproduct.elements.productPrice.value = products[pid].price;
+        editproduct.elements.productDescription.value = products[pid].description;
+
+        editproduct.addEventListener("submit", (e) => {             
+            e.preventDefault();
+            products[pid].id = editproduct.productID.value;
+            products[pid].name = editproduct.productName.value;
+            products[pid].price = editproduct.productPrice.value;
+            products[pid].description = editproduct.productDescription.value;
+            localStorage.setItem("products", JSON.stringify(products));
+            window.location.href = "/";
+        })
+    }, 100)
 }
